1d28896de447c22f7a482886be7c5cb1a8aa58b0
Alastair May <alastairmay4@gmail.com>
2023-03-15 10:14:38 -0300

    Added helper bat files (note: clone.bat should be moved to the directory outside the ridelink folder if you want to use it)

---
+++ install_all.bat
@@ -0,0 +1,2 @@
+call install_backend_req.bat
+call install_frontend_req.bat
---
+++ install_frontend_req.bat
@@ -0,0 +1,5 @@
+echo "Remember to run this as administrator if there are problems!"
+cd frontend
+#cmd /c requirements.txt
+npm i
+pause

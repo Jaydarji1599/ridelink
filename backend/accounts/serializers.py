165d68ced824638e5a7f7be70f6f2ee6fc4770ed
Jay Darji <93830277+Jaydarji1599@users.noreply.github.com>
2023-04-04 18:50:40 -0300

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -3,19 +3,20 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from .models import Passenger, Ride, Review, Contact
 
-# User Serializer
+# Serializer for User model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'date_joined')
 
-# Register Serializer
+# Serializer for user registration
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True}}
 
+    # Create and save a new user instance with the provided data
     def create(self, validated_data):
         user = User.objects.create_user(
             validated_data['username'],
@@ -27,18 +28,19 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         return user
 
-# Login Serializer
+# Serializer for user login
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
 
+    # Validate user credentials
     def validate(self, data):
         user = authenticate(**data)
         if user and user.is_active:
             return user
         raise serializers.ValidationError("Incorrect Credentials")
 
-
+# Serializer for updating user information
 class UpdateUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
 
@@ -50,21 +52,21 @@ class UpdateUserSerializer(serializers.ModelSerializer):
             'last_name': {'required': True},
         }
 
-
+    # Validate email uniqueness
     def validate_email(self, value):
         user = self.context['request'].user
         if User.objects.exclude(pk=user.pk).filter(email=value).exists():
             raise serializers.ValidationError({"email": "This email is already in use."})
         return value
 
-
+    # Validate username uniqueness
     def validate_username(self, value):
         user = self.context['request'].user
         if User.objects.exclude(pk=user.pk).filter(username=value).exists():
             raise serializers.ValidationError({"username": "This username is already in use."})
         return value
 
-
+    # Update user instance with the provided data
     def update(self, instance, validated_data):
         instance.first_name = validated_data['first_name']
         instance.last_name = validated_data['last_name']
@@ -75,7 +77,7 @@ class UpdateUserSerializer(serializers.ModelSerializer):
 
         return instance
 
-
+# Serializer for Ride model
 class RideSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ride
@@ -88,25 +90,21 @@ class RideSerializer(serializers.ModelSerializer):
             'driver',
         )
 
-
+# Serializer for Review model
 class ReviewSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Review
         fields = ('id', 'driver', 'reviewer', 'score')
 
-
+# Serializer for Contact model
 class ContactSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Contact
         fields = ('user', 'phone')
 
-
+# Serializer for Passenger model
 class PassengerSerializer(serializers.ModelSerializer):
     phone = serializers.CharField(read_only=True, source='user.contact.phone')
     class Meta:
         model = Passenger
         fields = ('id', 'user', 'ride', 'phone')
-
- 
